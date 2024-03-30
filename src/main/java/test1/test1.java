fd92142647c8d2b5ca20d465a465ded7c417408a
David Garcia Hernandez <david.garciahz@gmail.com>
2024-03-30 13:07:33 +0100

    Commit from Eclipse eGit 13.07

---
+++ src/main/java/test1/test1.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Hola mundo 2024.03.30 - 12.03");
+	}
+
+}
