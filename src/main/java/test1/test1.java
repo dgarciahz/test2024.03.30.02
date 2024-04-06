4a1d75af2c6c9a95e38ab3368d7e30281e4916f8
local_dgarciahz <david.garciahz@gmail.com>
2024-04-06 19:06:08 +0200

    branch_main v2

--- src/main/java/test1/test1.java
+++ src/main/java/test1/test1.java
@@ -5,6 +5,7 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.err.println("Hola mundo 2024.03.30 - 19.51 v2"); 
+		// branch main v2
 	}
 
 }
