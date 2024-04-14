d474cccab57deeba49b7c43bd58760e0457f191f
local_dgarciahz <david.garciahz@gmail.com>
2024-04-14 10:23:22 +0200

    Workflow2 added from Eclipse

--- src/main/java/test1/test1.java
+++ src/main/java/test1/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.err.println("Hola mundo 2024.04.07 - 10.40 v1"); 
+		System.err.println("Hola mundo 2024.04.07 - 10.40 v1");
 		// branch feature_branch v1 10.40
 	}
 
