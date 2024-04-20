5e5e02972b80103e4322376b3b06fefae84535e8
local_dgarciahz <david.garciahz@gmail.com>
2024-04-20 17:14:01 +0200

    mvn multipaso con surfire

--- src/test/java/testing_bytestNG/Test_New1.java
+++ src/test/java/testing_bytestNG/Test_New1.java
@@ -3,7 +3,7 @@ package testing_bytestNG;
 import org.testng.annotations.Test;
 import test1.test;
 
-public class NewTest1 {
+public class Test_New1 {
   @Test
   public void f() {
 	  assert(! new test().getMessage().equals(""));
