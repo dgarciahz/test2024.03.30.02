a10fc890c218b5902de1a8b9e4ee638e687d0fdb
local_dgarciahz <david.garciahz@gmail.com>
2024-04-28 21:25:55 +0200

    artifact and output results

--- src/main/java/test1/test.java
+++ src/main/java/test1/test.java
@@ -1,12 +1,24 @@
 package test1;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.Properties;
+
 public class test {
 
 	public static String _message="test string";
 	public String getMessage() {
 		return _message;
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+//		test1 t=new test1();
+//		t.readPropFile();
+//		String temp=t.readed_currentTimeMillis;
+//		File archivo = new File("CTM_"+temp+".txt");       
+//        OutputStream outputStream = new FileOutputStream(archivo);
+//        Properties prop = new Properties();
+//        prop.save(outputStream, temp);
 		// TODO Auto-generated method stub
 		// 2024.03.31 - 20.27 v2
 	}
