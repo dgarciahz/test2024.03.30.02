f0be437debdcf5bcbaa37f5e09333ba84d7bfe7a
local_dgarciahz <david.garciahz@gmail.com>
2024-04-28 12:47:01 +0200

    random value added

--- src/test/java/testing_bytestNG/Test_New1.java
+++ src/test/java/testing_bytestNG/Test_New1.java
@@ -18,6 +18,11 @@ public class Test_New1 {
 	  
 	  test1 t=new test1();
 	  t.readPropFile();
+	  System.out.println();	  
+	  System.out.println();	  
+	  System.out.println(t.readed_currentTimeMillis);
+	  System.out.println();	  
+	  System.out.println();	  
 	  
 	  test1 t1=new test1();
 	  Properties p1=t1.read_write_PropFile();
@@ -28,14 +33,20 @@ public class Test_New1 {
 	  
 	  String aux2=p1.getProperty("currentTimeMillis");
 	  assert(aux2!=null);
+	  System.out.println(aux2);
 	  assert(!t.readed_currentTimeMillis.equals(aux2));
+	  System.out.println();	  
+	  System.out.println();	  
 	  
 	  test1 t2=new test1();
 	  Properties p2=t2.read_write_PropFile();
 	  String aux3=p2.getProperty("db.user");
 	  assert(aux3.equals(aux1));
 	  String aux4=p2.getProperty("currentTimeMillis");
-	  assert(!aux4.equals(aux2));
+	  System.out.println(aux4);
+	  assert(!aux4.equals(p1.getProperty("currentTimeMillis")));
+	  System.out.println();	  
+	  System.out.println();	  
 	  
   }
 }
